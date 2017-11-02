a8c59dc9717db411f086bd0dee52d39f1afcf073
SeungpilPark <sppark@uengine.org>
2017-11-02 17:23:45 +0900

    Intiate

--- src/main/java/org.uengine/cloud/Application.java
+++ src/main/java/org.uengine/cloud/Application.java
@@ -1,14 +1,29 @@
 package org.uengine.cloud;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by uengine on 2017. 10. 5..
  */
 @SpringBootApplication
+@RestController
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
 public class Application {
+
+    @RequestMapping("/health")
+    public String health() {
+        return "";
+    }
 
     public static void main(String[] args) {
         new SpringApplicationBuilder(Application.class).web(true).run(args);
