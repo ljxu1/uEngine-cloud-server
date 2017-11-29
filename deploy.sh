506a2c8afd15f9860d2f2235bf9b46eea74ef916
darkgodarkgo@gmail.com <darkgodarkgo@gmail.com>
2017-11-29 10:04:42 +0900

    docker image name

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 docker login
 mvn package -B
-docker build -t sppark/uengine-cloud-ui:latest ./
-docker push sppark/uengine-cloud-ui:latest
+docker build -t sppark/uengine-cloud-server:latest ./
+docker push sppark/uengine-cloud-server:latest
